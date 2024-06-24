a725d004c143fbd1f2d2cee2523177924ed4a3bb
mariotbg11 <christofell.m@gmail.com>
2024-06-24 18:17:14 +0700

    Develop home screen layout

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,23 +1,54 @@
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import HomeStackNavigation from '../navigations/HomeStackNavigation'
-import MovieDetail from './MovieDetail'
+import { ScrollView, View, StatusBar, StyleSheet } from 'react-native'
+import type { MovieListProps } from '../types/app'
+import MovieList from '../components/movies/MovieList'
 
-const Stack = createNativeStackNavigator()
+const movieLists: MovieListProps[] = [
+  {
+    title: 'Now Playing in Theater',
+    path: 'movie/now_playing?language=en-US&page=1',
+    coverType: 'backdrop',
+  },
+  {
+    title: 'Upcoming Movies',
+    path: 'movie/upcoming?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Top Rated Movies',
+    path: 'movie/top_rated?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Popular Movies',
+    path: 'movie/popular?language=en-US&page=1',
+    coverType: 'poster',
+  },
+]
 
 export default function Home(): JSX.Element {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="HomeStackNavigation"
-        component={HomeStackNavigation}
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-      <Stack.Screen
-        name="MovieDetail"
-        component={MovieDetail}
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-    </Stack.Navigator>
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
+      </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight ?? 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+    rowGap: 16,
+  },
+})
